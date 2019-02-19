b386b841fa670716ff320e577b5e9fb6306ecdd5
Lawrence <mail@lawben.com>
2019-02-19 11:19:00 +0100

    Add javadocs for TestTopology

--- fluent-kafka-streams-tests/src/main/java/com/bakdata/fluent_kafka_streams_tests/TestTopology.java
+++ fluent-kafka-streams-tests/src/main/java/com/bakdata/fluent_kafka_streams_tests/TestTopology.java
@@ -25,6 +25,21 @@ import java.util.Properties;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+/**
+ * Represents the main interaction with Kafka for testing purposes. Handles all inputs and outputs of the
+ * {@link Topology} under test. This should be registered as an extension in your JUnit tests, to ensure that
+ * certain setup and teardown methods are called.
+ * Usage:
+ * {@code
+ *
+ * @RegisterExtension
+ * final TestTopology<Object, String> testTopology =
+ *     new TestTopology<>(app::getTopology, app.getKafkaProperties());
+ * }
+ *
+ * With {@code app} being any Kafka Streams application that you want to test.
+ *
+ */
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class TestTopology<DefaultK, DefaultV> implements BeforeEachCallback, AfterEachCallback {
@@ -39,6 +54,15 @@ public class TestTopology<DefaultK, DefaultV> implements BeforeEachCallback, Aft
     private final Properties properties = new Properties();
     private TopologyTestDriver testDriver;
 
+    /**
+     * Create a new {@link TestTopology} for your topology under test.
+     *
+     * @param topologyFactory Provides the topology under test. Ideally, this should always create a fresh topology to
+     *                        ensure strict separation of each test run.
+     * @param properties The properties of the Kafka Streams application under test.
+     *                   Required entries: APPLICATION_ID_CONFIG, BOOTSTRAP_SERVERS_CONFIG,
+     *                                     DEFAULT_KEY_SERDE_CLASS_CONFIG, DEFAULT_VALUE_SERDE_CLASS_CONFIG
+     */
     public TestTopology(final Function<? super Properties, ? extends Topology> topologyFactory, final Map<?, ?> properties) {
         this.topologyFactory = topologyFactory;
         this.properties.putAll(properties);
@@ -46,14 +70,141 @@ public class TestTopology<DefaultK, DefaultV> implements BeforeEachCallback, Aft
         this.defaultValueSerde = null;
     }
 
-    public TestTopology(final Topology topology, final Map<?, ?> properties) {
-        this((Properties) -> topology, properties);
-    }
-
+    /**
+     * See above constructor for more details.
+     * @param topologyFactory Provides the topology under test but does not require properties to build the topology.
+     */
     public TestTopology(final Supplier<? extends Topology> topologyFactory, final Map<?, ?> properties) {
         this((Properties) -> topologyFactory.get(), properties);
     }
 
+    /**
+     * See above constructor for more details.
+     * @param topology A fixed topology to be tested. This should only be used, if you are sure that the topology is not
+     *                 affected by other test runs. Otherwise, side-effects could impact your tests.
+     */
+    public TestTopology(final Topology topology, final Map<?, ?> properties) {
+        this((Properties) -> topology, properties);
+    }
+
+    /**
+     * Get all properties that the application has set.
+     */
+    public StreamsConfig getStreamsConfig() {
+        return new StreamsConfig(this.properties);
+    }
+
+    /**
+     * Get the default serde of the key type in your application.
+     */
+    private Serde<DefaultK> getDefaultKeySerde() {
+        return this.defaultKeySerde != null ? this.defaultKeySerde : this.getStreamsConfig().defaultKeySerde();
+    }
+
+    /**
+     * Get the default serde of the value type in your application.
+     */
+    private Serde<DefaultV> getDefaultValueSerde() {
+        return this.defaultValueSerde != null ? this.defaultValueSerde : this.getStreamsConfig().defaultValueSerde();
+    }
+
+
+    /**
+     * Get the only input topic used by the topology under test.
+     *
+     * @throws IllegalStateException if more than one input topic is present.
+     * @return {@link TestInput} of the input topic that you want to write to.
+     */
+    public TestInput<DefaultK, DefaultV> input() {
+        if (this.inputTopics.size() != 1) {
+            throw new IllegalStateException("#input() works with exactly 1 topic, if more are used," +
+                    " please use #input(String) to select a topic");
+        }
+        return this.input(this.inputTopics.iterator().next());
+    }
+
+    /**
+     * Get the input topic with the name `topic` used by the topology under test.
+     *
+     * @throws NoSuchElementException if there is no topic with that name.
+     * @return {@link TestInput} of the input topic that you want to write to.
+     */
+    public TestInput<DefaultK, DefaultV> input(final String topic) {
+        if (!this.inputTopics.contains(topic)) {
+            throw new NoSuchElementException(String.format("Input topic '%s' not found", topic));
+        }
+        return new TestInput<>(this.testDriver, topic, this.getDefaultKeySerde(), this.getDefaultValueSerde());
+    }
+
+    /**
+     * Get the only output topic used by the topology under test with
+     * {@link org.apache.kafka.streams.kstream.KStream}-semantics.
+     *
+     * @throws IllegalStateException if more than one output topic is present.
+     * @return {@link StreamOutput} of the output topic that you want to read from.
+     */
+    public TestOutput<DefaultK, DefaultV> streamOutput() {
+        if (this.outputTopics.size() != 1) {
+            throw new IllegalStateException("Please use #output(String) to select a topic");
+        }
+        return this.streamOutput(this.outputTopics.iterator().next());
+    }
+
+    /**
+     * Get the output topic with the name `topic` used by the topology under test with
+     * {@link org.apache.kafka.streams.kstream.KStream}-semantics.
+     *
+     * @throws NoSuchElementException if there is no topic with that name.
+     * @return {@link StreamOutput} of the output topic that you want to read from.
+     */
+    public TestOutput<DefaultK, DefaultV> streamOutput(final String topic) {
+        if (!this.outputTopics.contains(topic)) {
+            throw new NoSuchElementException(String.format("Output topic '%s' not found", topic));
+        }
+        return new StreamOutput<>(this.testDriver, topic, this.getDefaultKeySerde(), this.getDefaultValueSerde());
+    }
+
+    /**
+     * Get the only output topic used by the topology under test with
+     * {@link org.apache.kafka.streams.kstream.KTable}-semantics.
+     *
+     * @throws IllegalStateException if more than one output topic is present.
+     * @return {@link TableOutput} of the output topic that you want to read from.
+     */
+    public TestOutput<DefaultK, DefaultV> tableOutput() {
+        return this.streamOutput().asTable();
+    }
+
+    /**
+     * Get the output topic with the name `topic` used by the topology under test with
+     * {@link org.apache.kafka.streams.kstream.KTable}-semantics.
+     *
+     * @throws NoSuchElementException if there is no topic with that name.
+     * @return {@link TableOutput} of the output topic that you want to read from.
+     */
+    public TestOutput<DefaultK, DefaultV> tableOutput(final String topic) {
+        return this.streamOutput(topic).asTable();
+    }
+
+    /**
+     * TODO(aheise): anything to add?
+     * Get the interface to the schema registry.
+     */
+    public SchemaRegistryClient getSchemaRegistry() {
+        return this.schemaRegistry.getSchemaRegistryClient();
+    }
+
+    /**
+     * TODO(aheise): anything to add?
+     * Get the URL of the schema registry.
+     */
+    public String getSchemaRegistryUrl() {
+        return this.schemaRegistry.getUrl();
+    }
+
+    // ==================
+    // Non-public methods
+    // ==================
     private static void addExternalTopics(final Collection<String> allTopics, final String topics) {
         if (topics.contains("KSTREAM-") || topics.contains("KTABLE-")) {
             // Internal node created by Kafka. Not relevant for testing.
@@ -62,24 +213,12 @@ public class TestTopology<DefaultK, DefaultV> implements BeforeEachCallback, Aft
 
         // TODO: support wildcards
         if (topics.startsWith("[")) {
-            // a list of topics in the form of [topic1,...,topicN]
+            // A list of topics in the form of [topic1,...,topicN]
             allTopics.addAll(Arrays.asList(topics.substring(1, topics.length() - 1).split(",")));
         } else {
             // Only one topic present without leading '['
             allTopics.add(topics);
         }
-    }
-
-    public StreamsConfig getStreamsConfig() {
-        return new StreamsConfig(this.properties);
-    }
-
-    private Serde<DefaultK> getDefaultKeySerde() {
-        return this.defaultKeySerde != null ? this.defaultKeySerde : this.getStreamsConfig().defaultKeySerde();
-    }
-
-    private Serde<DefaultV> getDefaultValueSerde() {
-        return this.defaultValueSerde != null ? this.defaultValueSerde : this.getStreamsConfig().defaultValueSerde();
     }
 
     @Override
@@ -107,50 +246,5 @@ public class TestTopology<DefaultK, DefaultV> implements BeforeEachCallback, Aft
                 }
             }
         }
-    }
-
-    public TestInput<DefaultK, DefaultV> input() {
-        if (this.inputTopics.size() != 1) {
-            throw new IllegalStateException("#input() works with exactly 1 topic, if more are used," +
-                    " please use #input(String) to select a topic");
-        }
-        return this.input(this.inputTopics.iterator().next());
-    }
-
-    public TestInput<DefaultK, DefaultV> input(final String topic) {
-        if (!this.inputTopics.contains(topic)) {
-            throw new NoSuchElementException(String.format("Input topic '%s' not found", topic));
-        }
-        return new TestInput<>(this.testDriver, topic, this.getDefaultKeySerde(), this.getDefaultValueSerde());
-    }
-
-    public TestOutput<DefaultK, DefaultV> streamOutput() {
-        if (this.outputTopics.size() != 1) {
-            throw new IllegalStateException("Please use #output(String) to select a topic");
-        }
-        return this.streamOutput(this.outputTopics.iterator().next());
-    }
-
-    public TestOutput<DefaultK, DefaultV> streamOutput(final String topic) {
-        if (!this.outputTopics.contains(topic)) {
-            throw new NoSuchElementException(String.format("Output topic '%s' not found", topic));
-        }
-        return new StreamOutput<>(this.testDriver, topic, this.getDefaultKeySerde(), this.getDefaultValueSerde());
-    }
-
-    public TestOutput<DefaultK, DefaultV> tableOutput() {
-        return this.streamOutput().asTable();
-    }
-
-    public TestOutput<DefaultK, DefaultV> tableOutput(final String topic) {
-        return this.streamOutput(topic).asTable();
-    }
-
-    public SchemaRegistryClient getSchemaRegistry() {
-        return this.schemaRegistry.getSchemaRegistryClient();
-    }
-
-    public String getSchemaRegistryUrl() {
-        return this.schemaRegistry.getUrl();
     }
 }
