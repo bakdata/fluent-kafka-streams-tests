292ac1d706983e52ea988fc86c0f61ddd7ce8a64
Jordan Moore <crikket.007@gmail.com>
2019-04-11 20:12:47 -0500

    Add more than one schema to verify getLatest behavior

--- schema-registry-mock/src/test/java/com/bakdata/schemaregistrymock/SchemaRegistryMockTest.java
+++ schema-registry-mock/src/test/java/com/bakdata/schemaregistrymock/SchemaRegistryMockTest.java
@@ -28,10 +28,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.confluent.kafka.schemaregistry.client.SchemaMetadata;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -93,18 +95,25 @@ class SchemaRegistryMockTest {
 
     @Test
     void shouldHaveLatestSchemaVersion() throws IOException, RestClientException {
-        final Schema valueSchema = this.createSchema("value_schema");
+        final Schema valueSchema1 = this.createSchema("value_schema");
         final String topic = "test-topic";
-        final int id = this.schemaRegistry.registerValueSchema(topic, valueSchema);
+        final int id1 = this.schemaRegistry.registerValueSchema(topic, valueSchema1);
+
+        List<Schema.Field> fields = Collections.singletonList(
+                new Schema.Field("f1", Schema.create(Schema.Type.STRING), "", (Object) null));
+        final Schema valueSchema2 = Schema.createRecord("value_schema", "no doc", "", false, fields);
+        final int id2 = this.schemaRegistry.registerValueSchema(topic, valueSchema2);
 
         final List<Integer> versions = this.schemaRegistry.getSchemaRegistryClient().getAllVersions(topic + "-value");
-        assertThat(versions.size()).isNotZero();
+        assertThat(versions.size()).isEqualTo(2);
 
         final SchemaMetadata metadata = this.schemaRegistry.getSchemaRegistryClient().getLatestSchemaMetadata(topic + "-value");
-        assertThat(metadata.getId()).isEqualTo(id);
+        int metadataId = metadata.getId();
+        assertThat(metadataId).isNotEqualTo(id1);
+        assertThat(metadataId).isEqualTo(id2);
         final String schemaString = metadata.getSchema();
         final Schema retrievedSchema = new Schema.Parser().parse(schemaString);
-        assertThat(retrievedSchema).isEqualTo(valueSchema);
+        assertThat(retrievedSchema).isEqualTo(valueSchema2);
     }
 
     private Schema createSchema(final String name) {
