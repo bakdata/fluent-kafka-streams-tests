c9ff13d14ad5be8f2a10494edf3ca352c676d19e
Jordan Moore <crikket.007@gmail.com>
2019-04-17 15:01:21 -0500

    (#19) Mock subject version endpoints (#20)
    * (#19) Mock subject version endpoints
    
    * Add more than one schema to verify getLatest behavior
    
    * Update subject-version endpoint name

--- schema-registry-mock/src/test/java/com/bakdata/schemaregistrymock/SchemaRegistryMockTest.java
+++ schema-registry-mock/src/test/java/com/bakdata/schemaregistrymock/SchemaRegistryMockTest.java
@@ -25,10 +25,15 @@ package com.bakdata.schemaregistrymock;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.confluent.kafka.schemaregistry.client.SchemaMetadata;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+
 import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -70,6 +75,45 @@ class SchemaRegistryMockTest {
 
         final Schema retrievedSchema = this.schemaRegistry.getSchemaRegistryClient().getById(id);
         assertThat(retrievedSchema).isEqualTo(valueSchema);
+    }
+
+    @Test
+    void shouldHaveSchemaVersions() throws IOException, RestClientException {
+        final Schema valueSchema = this.createSchema("value_schema");
+        final String topic = "test-topic";
+        final int id = this.schemaRegistry.registerValueSchema(topic, valueSchema);
+
+        final List<Integer> versions = this.schemaRegistry.getSchemaRegistryClient().getAllVersions(topic + "-value");
+        assertThat(versions.size()).isOne();
+
+        final SchemaMetadata metadata = this.schemaRegistry.getSchemaRegistryClient().getSchemaMetadata(topic + "-value", versions.get(0));
+        assertThat(metadata.getId()).isEqualTo(id);
+        final String schemaString = metadata.getSchema();
+        final Schema retrievedSchema = new Schema.Parser().parse(schemaString);
+        assertThat(retrievedSchema).isEqualTo(valueSchema);
+    }
+
+    @Test
+    void shouldHaveLatestSchemaVersion() throws IOException, RestClientException {
+        final Schema valueSchema1 = this.createSchema("value_schema");
+        final String topic = "test-topic";
+        final int id1 = this.schemaRegistry.registerValueSchema(topic, valueSchema1);
+
+        List<Schema.Field> fields = Collections.singletonList(
+                new Schema.Field("f1", Schema.create(Schema.Type.STRING), "", (Object) null));
+        final Schema valueSchema2 = Schema.createRecord("value_schema", "no doc", "", false, fields);
+        final int id2 = this.schemaRegistry.registerValueSchema(topic, valueSchema2);
+
+        final List<Integer> versions = this.schemaRegistry.getSchemaRegistryClient().getAllVersions(topic + "-value");
+        assertThat(versions.size()).isEqualTo(2);
+
+        final SchemaMetadata metadata = this.schemaRegistry.getSchemaRegistryClient().getLatestSchemaMetadata(topic + "-value");
+        int metadataId = metadata.getId();
+        assertThat(metadataId).isNotEqualTo(id1);
+        assertThat(metadataId).isEqualTo(id2);
+        final String schemaString = metadata.getSchema();
+        final Schema retrievedSchema = new Schema.Parser().parse(schemaString);
+        assertThat(retrievedSchema).isEqualTo(valueSchema2);
     }
 
     private Schema createSchema(final String name) {
