64cc52cc2ee01f7c77b504e4af5a92c5293b6f10
Torben Meyer <torben.meyer@bakdata.com>
2021-03-31 13:23:58 +0200

    Add protobuf test case

--- fluent-kafka-streams-tests/build.gradle.kts
+++ fluent-kafka-streams-tests/build.gradle.kts
@@ -1,8 +1,14 @@
+import com.google.protobuf.gradle.*
+import org.gradle.kotlin.dsl.provider.gradleKotlinDslOf
+
 plugins {
     id("com.commercehub.gradle.plugin.avro") version "0.16.0"
+    id("com.google.protobuf") version "0.8.15"
+    java
 }
 
 description = "Provides the fluent Kafka Streams test framework."
+
 
 dependencies {
     val kafkaVersion: String by project
@@ -15,4 +21,17 @@ dependencies {
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junit5Version)
     testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junit5Version)
     testImplementation(group = "org.apache.avro", name = "avro", version = "1.8.2")
+    val confluentVersion: String by project
+    testImplementation(group = "io.confluent", name = "kafka-protobuf-provider", version = confluentVersion)
+    testImplementation(group= "io.confluent", name = "kafka-streams-protobuf-serde", version = confluentVersion)
+    testImplementation(group = "com.google.protobuf", name = "protobuf-java", version = ":3.15.3")
+}
+
+
+
+protobuf {
+    protoc {
+        // The artifact spec for the Protobuf Compiler
+        artifact = "com.google.protobuf:protoc:3.6.1"
+    }
 }
