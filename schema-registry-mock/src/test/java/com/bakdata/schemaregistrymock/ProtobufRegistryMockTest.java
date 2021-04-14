ad7cae75d9f5d9a567dba9d59687f2d29c8485bd
Torben Meyer <torben.meyer@bakdata.com>
2021-04-14 07:09:52 +0200

    Review clean up

--- schema-registry-mock/src/test/java/com/bakdata/schemaregistrymock/ProtobufRegistryMockTest.java
+++ schema-registry-mock/src/test/java/com/bakdata/schemaregistrymock/ProtobufRegistryMockTest.java
@@ -19,16 +19,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ProtobufRegistryMockTest {
-    private final SchemaRegistryMock schemaRegistry = new SchemaRegistryMock(List.of(new ProtobufSchemaProvider()));
-    private final ParsedSchema schema = createSchema();
+class ProtobufRegistryMockTest {
+    private final SchemaRegistryMock schemaRegistry;
+    private final ParsedSchema schema;
 
-    public ProtobufRegistryMockTest() throws IOException {
-    }
-
-    private static ProtobufSchema createSchema() throws IOException {
+    ProtobufRegistryMockTest() throws IOException {
+        this.schemaRegistry = new SchemaRegistryMock(List.of(new ProtobufSchemaProvider()));
+        
         final Path resources = Path.of("src", "test", "resources");
-        return new ProtobufSchema(Files.readString(resources.resolve("record.proto")));
+        this.schema = new ProtobufSchema(Files.readString(resources.resolve("record.proto")));
     }
 
     @BeforeEach
@@ -81,8 +80,7 @@ public class ProtobufRegistryMockTest {
         final List<Integer> versions = this.schemaRegistry.getSchemaRegistryClient().getAllVersions(topic + "-value");
         assertThat(versions.size()).isOne();
 
-        final SchemaMetadata
-                metadata =
+        final SchemaMetadata metadata =
                 this.schemaRegistry.getSchemaRegistryClient().getSchemaMetadata(topic + "-value", versions.get(0));
         assertThat(metadata.getId()).isEqualTo(id);
         final String schemaString = metadata.getSchema();
@@ -92,8 +90,8 @@ public class ProtobufRegistryMockTest {
 
     @Test
     void shouldReturnAllSubjects() throws IOException, RestClientException {
-        this.schemaRegistry.registerKeySchema("test-topic", createSchema());
-        this.schemaRegistry.registerValueSchema("test-topic", createSchema());
+        this.schemaRegistry.registerKeySchema("test-topic", this.schema);
+        this.schemaRegistry.registerValueSchema("test-topic", this.schema);
         final Collection<String> allSubjects = this.schemaRegistry.getSchemaRegistryClient().getAllSubjects();
         assertThat(allSubjects).hasSize(2).containsExactly("test-topic-key", "test-topic-value");
     }
@@ -101,7 +99,7 @@ public class ProtobufRegistryMockTest {
 
     @Test
     void shouldDeleteKeySchema() throws IOException, RestClientException {
-        this.schemaRegistry.registerKeySchema("test-topic", createSchema());
+        this.schemaRegistry.registerKeySchema("test-topic", this.schema);
         final SchemaRegistryClient client = this.schemaRegistry.getSchemaRegistryClient();
         final Collection<String> allSubjects = client.getAllSubjects();
         assertThat(allSubjects).hasSize(1).containsExactly("test-topic-key");
@@ -113,7 +111,7 @@ public class ProtobufRegistryMockTest {
     @Test
     void shouldDeleteValueSchema() throws IOException, RestClientException {
         final SchemaRegistryClient client = this.schemaRegistry.getSchemaRegistryClient();
-        this.schemaRegistry.registerValueSchema("test-topic", createSchema());
+        this.schemaRegistry.registerValueSchema("test-topic", this.schema);
         final Collection<String> allSubjects = client.getAllSubjects();
         assertThat(allSubjects).hasSize(1).containsExactly("test-topic-value");
         this.schemaRegistry.deleteValueSchema("test-topic");
@@ -124,7 +122,7 @@ public class ProtobufRegistryMockTest {
     @Test
     void shouldDeleteKeySchemaWithClient() throws IOException, RestClientException {
         final SchemaRegistryClient client = this.schemaRegistry.getSchemaRegistryClient();
-        this.schemaRegistry.registerKeySchema("test-topic", createSchema());
+        this.schemaRegistry.registerKeySchema("test-topic", this.schema);
         final Collection<String> allSubjects = client.getAllSubjects();
         assertThat(allSubjects).hasSize(1).containsExactly("test-topic-key");
         client.deleteSubject("test-topic-key");
@@ -135,7 +133,7 @@ public class ProtobufRegistryMockTest {
     @Test
     void shouldDeleteValueSchemaWithClient() throws IOException, RestClientException {
         final SchemaRegistryClient client = this.schemaRegistry.getSchemaRegistryClient();
-        this.schemaRegistry.registerValueSchema("test-topic", createSchema());
+        this.schemaRegistry.registerValueSchema("test-topic", this.schema);
         final Collection<String> allSubjects = client.getAllSubjects();
         assertThat(allSubjects).hasSize(1).containsExactly("test-topic-value");
         client.deleteSubject("test-topic-value");
@@ -145,9 +143,8 @@ public class ProtobufRegistryMockTest {
 
     @Test
     void shouldNotHaveSchemaVersionsForDeletedSubject() throws IOException, RestClientException {
-        final ParsedSchema valueSchema = createSchema();
         final String topic = "test-topic";
-        final int id = this.schemaRegistry.registerValueSchema(topic, valueSchema);
+        final int id = this.schemaRegistry.registerValueSchema(topic, this.schema);
 
         final List<Integer> versions = this.schemaRegistry.getSchemaRegistryClient().getAllVersions(topic + "-value");
         assertThat(versions.size()).isOne();
