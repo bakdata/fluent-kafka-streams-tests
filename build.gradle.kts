74c31ff01db03d3987301293e98ecad9a783e429
Philipp Schirmer <philipp.schirmer@bakdata.com>
2021-09-13 17:23:41 +0200

    Fix changelog generation (#55)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("net.researchgate.release") version "2.8.1"
     id("com.bakdata.sonar") version "1.1.7"
     id("com.bakdata.sonatype") version "1.1.7"
-    id("org.hildan.github.changelog") version "1.7.0"
+    id("org.hildan.github.changelog") version "0.8.0"
     id("io.freefair.lombok") version "5.3.3.3" apply false
 }
 
