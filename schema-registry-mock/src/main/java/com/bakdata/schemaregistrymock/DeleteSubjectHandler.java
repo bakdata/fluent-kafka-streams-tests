a3864d3f3754a33a91ea5c7a3f87b9b0ba54886e
Torben Meyer <torben.meyer@bakdata.com>
2023-11-03 11:43:36 +0100

    Update copyright

--- schema-registry-mock/src/main/java/com/bakdata/schemaregistrymock/DeleteSubjectHandler.java
+++ schema-registry-mock/src/main/java/com/bakdata/schemaregistrymock/DeleteSubjectHandler.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2023 bakdata GmbH
+ * Copyright (c) 2023 bakdata
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
