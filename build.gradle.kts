b619a424d372daa1eab08157ae47cb9e30cde052
Philipp Schirmer <philipp.schirmer@bakdata.com>
2024-02-21 17:16:47 +0100

    Upgrade to Gradle 8.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     // release
     id("net.researchgate.release") version "3.0.2"
-    id("com.bakdata.sonar") version "1.1.11"
-    id("com.bakdata.sonatype") version "1.1.11"
-    id("org.hildan.github.changelog") version "1.12.1"
-    id("io.freefair.lombok") version "6.6.1" apply false
+    id("com.bakdata.sonar") version "1.1.16"
+    id("com.bakdata.sonatype") version "1.1.14"
+    id("org.hildan.github.changelog") version "2.2.0"
+    id("io.freefair.lombok") version "8.4" apply false
 }
 
 allprojects {
@@ -51,9 +51,10 @@ configure<org.hildan.github.changelog.plugin.GitHubChangelogExtension> {
 subprojects {
     apply(plugin = "java-library")
     apply(plugin = "io.freefair.lombok")
-    configure<JavaPluginExtension> {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(11)
+        }
     }
 
     tasks.withType<Javadoc> {
