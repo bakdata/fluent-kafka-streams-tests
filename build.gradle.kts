391ca696cb273df298ef857f4bf041a3aa91f7d8
Philipp Schirmer <philipp.schirmer@bakdata.com>
2024-07-19 12:58:57 +0200

    Fix missing kassava dependency in plugins (#95)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     // release
     id("com.bakdata.release") version "1.4.0"
     id("com.bakdata.sonar") version "1.4.0"
-    id("com.bakdata.sonatype") version "1.4.0"
+    id("com.bakdata.sonatype") version "1.4.1"
     id("io.freefair.lombok") version "8.4" apply false
 }
 
