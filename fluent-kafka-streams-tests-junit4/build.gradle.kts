dd740b14e6aa3715628810a789fa68f7e8f47067
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-06-19 18:08:53 +0200

    Migrate to version catalog (#108)

--- fluent-kafka-streams-tests-junit4/build.gradle.kts
+++ fluent-kafka-streams-tests-junit4/build.gradle.kts
@@ -3,9 +3,7 @@ description = "Provides the fluent Kafka Streams test framework."
 dependencies {
     api(project(":fluent-kafka-streams-tests"))
 
-    val junit4Version: String by project
-    api(group = "junit", name = "junit", version = junit4Version)
-    testImplementation(group = "junit", name = "junit", version = junit4Version)
+    api(libs.junit4)
 }
 
 tasks.test {
--- fluent-kafka-streams-tests-junit5/build.gradle.kts
+++ fluent-kafka-streams-tests-junit5/build.gradle.kts
@@ -3,8 +3,6 @@ description = "Provides the fluent Kafka Streams test framework."
 dependencies {
     api(project(":fluent-kafka-streams-tests"))
 
-    val junit5Version: String by project
-    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junit5Version)
-    testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junit5Version)
-    api(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junit5Version)
+    testRuntimeOnly(libs.junit.platform.launcher)
+    api(libs.junit.jupiter)
 }
